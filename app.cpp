c0514fcc7a20f23d927a61e8f0536919abd2d8f2
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-04-14 12:34:20 +0300

    add the Smaple opp

--- app.cpp
+++ app.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
 using namespace std;
 
-class Employee {
+class AbstractEmployee{
+    virtual void AskForPromotion()=0;
+};
+
+class Employee:AbstractEmployee {
 private:
     string Name;
     string Company;
     int Age;
 
 public:
-    void
+    void setName(string name){ //setter
+        Name=name;
+    }
+    string getName(){ //getter
+        return Name;
+    }
+    void setCompnay(string company){
+        Company=company;
+    }
+    string getCompany(){
+        return Company;
+    }
+    void setAge(int age){
+        Age=age;
+    }
+    int getAge(){
+        return Age;
+    }
     Employee(string name,string company,int age){
         Name = name;
         Company=company;
@@ -20,13 +41,20 @@ public:
         cout<<"Company - "<<Company<<endl;
         cout<<"Age - "<<Age<<endl;
     }
+    void AskForPromotion(){
+        if(Age > 30)
+            cout<<Name<<" got promoted"<<endl;
+        else
+            cout<<Name<<", sorry no promoted for you! "<<endl;
+        
+    }
 };
 
 int main()
 {
-    Employee employee1=Employee("Mohamed","MKee",18);
-    employee1.IntroduceYourself();
-    Employee employee2=Employee("Ali","ID8",35);
-    employee2.IntroduceYourself();
-    cout<<"\n";
+    Employee employee1= Employee("Mohamed","MKee",18);
+    Employee employee2= Employee("Ali","Apple",35);
+
+    employee1.AskForPromotion();
+    employee2.AskForPromotion();
 }
