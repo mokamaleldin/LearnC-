161cfa7981bc730706ed0fd569f2992907387aec
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-04-14 12:48:47 +0300

    End with OOP

---
+++ Employee.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+//Abstraction
+class AbstractEmployee{
+    virtual void AskForPromotion()=0;
+};
+
+class Employee:AbstractEmployee {
+private:
+    string Name;
+    string Company;
+    int Age;
+
+public:
+    void setName(string name){ //setter
+        Name=name;
+    }
+    string getName(){ //getter
+        return Name;
+    }
+    void setCompnay(string company){
+        Company=company;
+    }
+    string getCompany(){
+        return Company;
+    }
+    void setAge(int age){
+        Age=age;
+    }
+    int getAge(){
+        return Age;
+    }
+    Employee(string name,string company,int age){
+        Name = name;
+        Company=company;
+        Age=age;
+    }
+    void IntroduceYourself(){
+        cout<<"Name - "<<Name<<endl;
+        cout<<"Company - "<<Company<<endl;
+        cout<<"Age - "<<Age<<endl;
+    }
+    void AskForPromotion(){
+        if(Age > 30)
+            cout<<Name<<" got promoted"<<endl;
+        else
+            cout<<Name<<", sorry no promoted for you! "<<endl;
+        
+    }
+};
+
+/*
+                **************************************
+                *                                    *
+                *                                    *
+                *         Mohamed Kamaleldin         *
+                *                                    *
+                *                                    *
+                **************************************
+
+*/
+
+int main()
+{
+    Employee employee1= Employee("Mohamed","MKee",18);
+    Employee employee2= Employee("Ali","Apple",35);
+
+    employee1.AskForPromotion();
+    employee2.AskForPromotion();
+}
