68b11e58b77333b93354eafeaccdfd95c26fc3fa
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-06-09 15:17:01 +0300

    Arithmetic And Array

--- pointer/p4.cpp
+++ pointer/p4.cpp
@@ -6,5 +6,20 @@ using namespace std;
 
 int main()
 {
-    cout << "hi";
+    int nums[]{10, 20, 30, 40};
+
+    cout << nums << endl; //1st Element => memory address
+    cout << &nums[0] << endl; //1st Element => memory address
+
+    cout << *nums << endl; //10
+    cout << nums[0] << endl; //10
+
+    int *ptr = nums;
+    cout << ptr << endl; //1st Element => memory address
+    cout << *ptr << endl; //1st Element => 10
+
+    ptr++;
+    cout << ptr << endl; // 2st Element => memory address
+    cout << *ptr << endl; //2st Element => 20
 }
+ 
