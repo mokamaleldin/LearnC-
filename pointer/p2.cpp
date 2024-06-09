91b3af3e4b4392e919ffe3a3aa34ff88343de5e4
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-06-09 14:50:51 +0300

    Pointing To Array

--- pointer/p2.cpp
+++ pointer/p2.cpp
@@ -6,5 +6,18 @@ using namespace std;
 
 int main()
 {
-    cout << "hi";
+    int nums[]{10, 20, 30, 40};
+    int *ptr = &nums[0];
+
+    cout << "first Element\n\n";
+    cout << "Value with index " << nums[0] << endl; //10
+    cout << "value with pointer: " << *ptr << endl; //10
+    cout << "Address with index " << &nums[0] << endl; //address
+    cout << "Address with pointer: " << ptr << endl; //address
+
+    cout << "\n\nsecond Element\n\n";
+    cout << "Value with index " << nums[1] << endl; //20
+    cout << "value with pointer: " << *(ptr+1) << endl; //20
+    cout << "Address with index " << &nums[1] << endl; //address
+    cout << "Address with pointer: " << ptr+1 << endl; //address
 }
