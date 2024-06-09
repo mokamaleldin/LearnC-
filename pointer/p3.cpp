1f0822e9e8e1d490363c70dccb5b43270e57cfa1
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-06-09 15:07:13 +0300

    Void And Wild Pointer And Null

--- pointer/p3.cpp
+++ pointer/p3.cpp
@@ -6,5 +6,18 @@ using namespace std;
 
 int main()
 {
-    cout << "hi";
+    int *ptr1; //wild pointer
+    int *ptr2=NULL; //wild pointer
+    int *ptr3=nullptr; // wild pointer
+
+    cout << ptr1 << endl; //Garbage value => it mean it can be any number
+    cout << ptr2 << endl; //0
+    cout << ptr3 << endl; //0
+
+    int a = 100;
+    void *ptr = &a;
+    cout << ptr << endl; //address
+     cout << *(int *)ptr << endl; //100 
+     //modern way
+     cout << *static_cast<int *>(ptr) << endl; //10
 }
